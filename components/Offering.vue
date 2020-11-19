017bc0eebbee48f5102aeb0f16b76e0eae641029
noriko takizawa <nt@henkelhiedl.com>
2020-11-19 21:56:24 +0100

    delete static/json/data.json add data text

--- components/Offering.vue
+++ components/Offering.vue
@@ -1,22 +1,15 @@
 <template>
   <div class="offering" id="offering">
     <SubmenuHeader
-    :barImg="data.headerImg"
-    :headerImgText="data.headerImgText"
-    :header="data.headerText"
+      :barImg="data.headerImg"
+      :headerImgText="data.headerImgText"
+      :header="data.headerText"
     />
-    <TextBox
-      :textHeader="data.text01_header"
-      :textContent="data.text01"
-    />
-    <Gallery
-      :imageHeader="data.galleryText"
-      :images="data.galleryImg"
-    />
-    <TextBox
-      :textHeader="data.text02_header"
-      :textContent="data.text02"
-    />
+    <TextBox :textHeader="data.text01_header" :textContent="data.text01" />
+    <Gallery :imageHeader="data.galleryText" :images="data.galleryImg" />
+    <TextBox :textHeader="data.text02_header" :textContent="data.text02" />
+    <TextBox :textHeader="data.text03_header" :textContent="data.text03" />
+    <TextBox :textHeader="data.text04_header" :textContent="data.text04" />
   </div>
 </template>
 
