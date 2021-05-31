c7b79996d48bd37a750ec5ac9f927e1206616e20
noriko-Takizawa <norikotakizawa87@gmail.com>
2021-05-31 22:08:11 +0200

    add: add meta content

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,7 @@ export default {
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || '',
+        content: 'Hier kannst du alles erfahren, was es rund um deine Bio-Gärtnerei an der Stadtgrenze zu Berlin zu wissen gibt',
       },
     ],
   },
